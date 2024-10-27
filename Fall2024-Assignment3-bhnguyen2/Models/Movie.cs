83162c0e0698f70f632077affe61287b6bb1fc8e
Brandon Nguyen <bhnguyen2@crimson.ua.edu>
2024-10-27 00:32:17 -0500

    intial setup with some model and controller stuff for movies and actors

---
+++ Fall2024-Assignment3-bhnguyen2/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace Fall2024_Assignment3_bhnguyen2.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string IMBD { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public byte[]? Photo { get; set; }
+    }
+}
