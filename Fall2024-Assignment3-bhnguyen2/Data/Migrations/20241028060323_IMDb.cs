3e9ab5fd1cf69e4e1109e7e410fa83554b976656
Brandon Nguyen <bhnguyen2@crimson.ua.edu>
2024-10-28 12:08:53 -0500

    fix database

---
+++ Fall2024-Assignment3-bhnguyen2/Data/Migrations/20241028060323_IMDb.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fall2024_Assignment3_bhnguyen2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IMDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IMBD",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "IMBD",
+                table: "Actor");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IMDb",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IMDb",
+                table: "Actor",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IMDb",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "IMDb",
+                table: "Actor");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IMBD",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IMBD",
+                table: "Actor",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Fall2024-Assignment3-bhnguyen2/Models/Actor.cs
+++ Fall2024-Assignment3-bhnguyen2/Models/Actor.cs
@@ -1,12 +1,16 @@
-﻿namespace Fall2024_Assignment3_bhnguyen2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fall2024_Assignment3_bhnguyen2.Models
 {
     public class Actor
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Gender { get; set; }
         public int Age { get; set; }
-        public string IMBD { get; set; }
+        public string? IMDb { get; set; }
         public byte[]? Photo { get; set; }
     }
 }
